f4cd68b672601b89020ca716c2d1fb1d6fd4cdb5
Yamaguchi Hiromitsu <yamagame@mac.com>
2021-08-22 08:19:31 +0900

    ステップキーワードを省略

--- tools/step-array.js
+++ tools/step-array.js
@@ -1,25 +1,31 @@
 const debugKeyword = "デバッグ停止";
 
 class StepArray extends Array {
+  static Given = "前提";
+  static Then = "ならば";
+  static When = "もし";
+  static And = "かつ";
+  static Comment = "#";
+
   constructor({ name, manual, action, params }) {
     super();
     if (manual !== debugKeyword && manual !== "") {
       manual.split("\n").forEach((manual) => {
-        this.step(
+        this.comment(
           manual,
           () => manual !== debugKeyword && manual !== "",
-          () => `# ${manual}`
+          () => `${manual}`
         );
       });
     }
-    this.step(
+    this.when(
       manual,
       () => manual === debugKeyword,
-      () => `もし ${manual}`
+      () => `${manual}`
     );
   }
 
-  step(value, isValid, callback) {
+  _step(value, step, isValid, callback) {
     if (callback === undefined) {
       callback = isValid;
       isValid = () => true;
@@ -32,18 +38,40 @@ class StepArray extends Array {
     ) {
       const _callback = callback || isValid;
       if (value.manual === debugKeyword) {
-        this.push(`もし ${value.manual}`);
+        this.push(`${StepArray.When} ${value.manual}`);
       }
       const r = _callback(value);
       if (typeof r === "string") {
-        this.push(r);
+        this.push(`${step} ${r}`);
       } else if (Array.isArray(r)) {
-        r.forEach((v) => this.push(v));
+        r.forEach((v) => this.push(`${step} ${v}`));
       }
+      return true;
     }
+    return false;
   }
 
-  add(step, action = "もし") {
+  given(value, isValid, callback) {
+    return this._step(value, StepArray.Given, isValid, callback);
+  }
+
+  then(value, isValid, callback) {
+    return this._step(value, StepArray.Then, isValid, callback);
+  }
+
+  when(value, isValid, callback) {
+    return this._step(value, StepArray.When, isValid, callback);
+  }
+
+  and(value, isValid, callback) {
+    return this._step(value, StepArray.And, isValid, callback);
+  }
+
+  comment(value, isValid, callback) {
+    return this._step(value, StepArray.Comment, isValid, callback);
+  }
+
+  add(step, action = StepArray.And) {
     this.push(`${action} ${step}`);
   }
 }
--- tools/steps/scenario-steps.js
+++ tools/steps/scenario-steps.js
@@ -3,25 +3,25 @@ module.exports = function (StepArray) {
     デフォルト: (screen) => {
       const { name, manual, action, params } = screen;
       const s = new StepArray(screen);
-      s.step(name, (t) => `ならば 「${name}」に遷移する`);
+      s.given(name, (t) => `「${name}」に遷移する`);
       Object.entries(params).forEach(([k, v]) => {
-        s.step(
+        s.when(
           v,
           (t) =>
-            `もし ${k}は${t.values
+            `${k}は${t.values
               .filter((v) => v !== "")
               .map((v) => `「${v}」`)
               .join("")}${t.mark !== "◯" ? ` "${t.mark}" ` : ""}を入力`
         );
       });
       s.add("スクリーンショットを撮る");
-      s.step(action, (t) => `もし ${action}`);
+      s.when(action, (t) => `${action}`);
       return s;
     },
 
     ページを開く: (screen) => {
       const s = new StepArray(screen);
-      s.step(screen, (t) => `前提 ページ「${t.values[0]}」を開く`);
+      s.given(screen, (t) => `ページ「${t.values[0]}」を開く`);
       s.add("スクリーンショットを撮る");
       return s;
     },
@@ -29,54 +29,48 @@ module.exports = function (StepArray) {
     編集画面: (screen) => {
       const { name, manual, action, params } = screen;
       const s = new StepArray(screen);
-      s.step(name, (t) => `ならば 「${name}」に遷移する`);
-      s.step(params["名前の変更"], (t) => `もし 名前は「${t.mark}」を入力`);
-      s.step(params["性別"], (t) => `もし 性別は「${t.values[0]}」を入力`);
-      s.step(params["住所"], (t) => {
+      s.then(name, (t) => `「${name}」に遷移する`);
+      s.when(params["名前の変更"], (t) => `名前は「${t.mark}」を入力`);
+      s.when(params["性別"], (t) => `性別は「${t.values[0]}」を入力`);
+      s.when(params["住所"], (t) => {
         if (t.values[2]) {
-          return `もし 住所は「${t.values[0]}」「${t.values[1]}」「${t.values[2]}」を入力`;
+          return `住所は「${t.values[0]}」「${t.values[1]}」「${t.values[2]}」を入力`;
         }
-        return `もし 住所は「${t.values[0]}」「${t.values[1]}」を入力`;
+        return `住所は「${t.values[0]}」「${t.values[1]}」を入力`;
       });
       s.add("スクリーンショットを撮る");
-      s.step(action, (t) => `もし ${action}`);
+      s.when(action, (t) => `${action}`);
       return s;
     },
 
     設定画面: (screen) => {
       const { name, manual, action, params } = screen;
       const s = new StepArray(screen);
-      s.step(name, () => `ならば 「${name}」に遷移する`);
-      s.step(
+      s.then(name, () => `「${name}」に遷移する`);
+      s.when(
         params["端末"],
         (t) => t.values[0] === "iPhone",
-        (t) => `もし 端末は「${t.values[0]}」、色は「${t.values[1]}」を選択`
+        (t) => `端末は「${t.values[0]}」、色は「${t.values[1]}」を選択`
       );
-      s.step(
+      s.when(
         params["端末"],
         (t) => t.values[0] === "Android",
-        (t) => `もし 端末は「${t.values[0]}」を選択`
+        (t) => `端末は「${t.values[0]}」を選択`
       );
-      s.step(params["容量"], (t) => `もし 容量は「${t.values[0]}」を選択`);
+      s.when(params["容量"], (t) => `容量は「${t.values[0]}」を選択`);
       s.add("スクリーンショットを撮る");
-      s.step(action, () => `もし ${action}`);
+      s.when(action, () => `${action}`);
       return s;
     },
 
     スタート画面: (screen) => {
       const { name, manual, action, params } = screen;
       const s = new StepArray(screen);
-      s.step(name, () => `ならば 「${name}」に遷移する`);
-      s.step(
-        params["アカウント"],
-        (t) => `もし アカウントは "${t.mark}" を入力`
-      );
-      s.step(
-        params["パスワード"],
-        (t) => `もし パスワードは "${t.mark}" を入力`
-      );
+      s.then(name, () => `「${name}」に遷移する`);
+      s.when(params["アカウント"], (t) => `アカウントは "${t.mark}" を入力`);
+      s.when(params["パスワード"], (t) => `パスワードは "${t.mark}" を入力`);
       s.add("スクリーンショットを撮る");
-      s.step(action, () => `もし ${action}`);
+      s.when(action, () => `${action}`);
       return s;
     },
   };
